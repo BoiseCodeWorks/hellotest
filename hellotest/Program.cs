65a563060aee1d53e4dc5398189d9ec4ea1bfab5
Jake Overall <joverall22@gmail.com>
2018-09-24 21:31:07 -0600

    number guessing

--- hellotest/Program.cs
+++ hellotest/Program.cs
@@ -6,7 +6,44 @@ namespace hellotest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Random random = new Random();
+            int answer = random.Next(1, 100);
+            var guesses = 0;
+            Console.WriteLine("Try guesing my number");
+
+            bool playing = true;
+            while (playing)
+            {
+                guesses++;
+                var guess = Console.ReadLine();
+
+
+                int.TryParse(guess, out int x);
+
+                if (x == answer)
+                {
+                    Console.WriteLine("GREAT GUESS");
+                    playing = false;
+                    break;
+                }
+
+                if (x > answer)
+                {
+                    Console.WriteLine("LOWER");
+                    continue;
+                }
+
+                if (x < answer)
+                {
+                    Console.WriteLine("HIGHER");
+                }
+
+            }
+
+            Console.WriteLine("Bye Felicia");
+
         }
     }
+
 }
